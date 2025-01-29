3a1577930ac796129b300af2ad00fecb166b033c
thoubou <thoubou@gmail.com>
2025-01-29 15:12:24 +0200

    Fixed constants

--- pythonProject/FedAvg5/constants.py
+++ pythonProject/FedAvg5/constants.py
@@ -3,7 +3,7 @@ from enum import Enum
 class AGG(Enum):
     AVG = "AVG"
     SUM = "SUM"
-    COUNT = "COUNT"
+    COUNT = "count"
     def __str__(self):
         return self.name
 
--- pythonProject/FedAvg5/flask_server.py
+++ pythonProject/FedAvg5/flask_server.py
@@ -4,10 +4,26 @@
 from flask import Flask, request, jsonify
 import sqlite3
 import os
-from constants import Database as meta
+from constants import Database,AGG
 app = Flask(__name__)
 
 DATABASE = "database.db"
+
+table_creation= '''
+                    CREATE TABLE IF NOT EXISTS aggregation_step (
+                    operation_id TEXT NOT NULL,
+                    round INTEGER NOT NULL,
+                    client_id TEXT NOT NULL,                    
+                    agg_func TEXT NOT NULL,
+                    value REAL NOT NULL,
+                    PRIMARY KEY (operation_id,client_id, round, agg_func)
+                    )
+                '''
+local_aggregation = '''INSERT INTO aggregation_step (operation_id,round,client_id,agg_func,value) VALUES (?,?,?,?,?)'''
+global_aggregation = '''SELECT sum(value)
+                                FROM aggregation_step
+                                WHERE operation_id=? AND round=? AND agg_func=?
+                                HAVING count(value)=?'''
 
 # Function to get a database connection
 def get_db_connection():
@@ -21,16 +37,7 @@ def initialize_database():
         connection = get_db_connection()
 
         cursor = connection.cursor()
-        cursor.execute('''
-                    CREATE TABLE IF NOT EXISTS aggregation_step (
-                    operation_id TEXT NOT NULL,
-                    round INTEGER NOT NULL,
-                    client_id TEXT NOT NULL,                    
-                    agg_func TEXT NOT NULL,
-                    value REAL NOT NULL,
-                    PRIMARY KEY (operation_id,client_id, round, agg_func)
-                    )
-                ''')
+        cursor.execute(table_creation)
         connection.commit()
         connection.close()
         print("Database initialized!")
@@ -38,24 +45,24 @@ def initialize_database():
 # Automatically initialize the database when the app starts
 initialize_database()
 
-def get_attribute(args, key:meta):
+def get_attribute(args, key:Database):
    return args.get(key.value)
 
 
 @app.route('/add-aggregation', methods=['POST'])
 def add_aggregation():
     data = request.args
-    operation_id =get_attribute(data, meta.OPP)
-    client_id = get_attribute(data, meta.CLIENT)
-    exec_round = get_attribute(data, meta.ROUND)
-    agg_func = get_attribute(data, meta.AGG)
-    value = get_attribute(data, meta.VALUE)
+    operation_id =get_attribute(data, Database.OPP)
+    client_id = get_attribute(data, Database.CLIENT)
+    exec_round = get_attribute(data, Database.ROUND)
+    agg_func = get_attribute(data, Database.AGG)
+    value = get_attribute(data, Database.VALUE)
     print("Adding Data from client: ",client_id,"round: ",exec_round,"agg_func: ",agg_func,"value: ",value,"")
     if not data or not client_id or not exec_round or not agg_func or not value :
         return jsonify({"error": "Filed is missing"}), 400
     connection = get_db_connection()
     cursor = connection.cursor()
-    cursor.execute('INSERT INTO aggregation_step (operation_id,round,client_id,agg_func,value) VALUES (?,?,?,?,?)', (operation_id,exec_round,client_id,agg_func,value))
+    cursor.execute(local_aggregation, (operation_id, exec_round, client_id, agg_func, value))
     connection.commit()
     connection.close()
     return jsonify({"message": "User added successfully!"}), 201
@@ -73,29 +80,23 @@ def get_aggregations():
 @app.route('/get_aggregation', methods=['GET'])
 def get_aggregation():
     data = request.args
-    operation_id = get_attribute(data, meta.OPP)
-    exec_round = get_attribute(data, meta.ROUND)
-    agg_func = get_attribute(data, meta.AGG)
-    client_count:int= int(get_attribute(data, meta.CLIENT_COUNT))
+    operation_id = get_attribute(data, Database.OPP)
+    exec_round = get_attribute(data, Database.ROUND)
+    agg_func = get_attribute(data, Database.AGG)
+    client_count:int= int(get_attribute(data, Database.CLIENT_COUNT))
 
     connection = get_db_connection()
     cursor = connection.cursor()
     _sum=None
     _count=None
     print(client_count)
-    if agg_func == 'sum' or agg_func == 'avg':
-        cursor.execute('''SELECT sum(value)
-                                FROM aggregation_step
-                                WHERE operation_id=? AND round=? AND agg_func='sum'
-                                HAVING count(value)=?''', (operation_id, exec_round, client_count))
+    if agg_func == AGG.SUM.value or agg_func == AGG.AVG.value:
+        cursor.execute(global_aggregation, (operation_id, exec_round,AGG.SUM.value, client_count))
         result = cursor.fetchone()
         if result:
             _sum = result[0]
-    if agg_func == 'count' or agg_func == 'avg':
-        cursor.execute('''SELECT sum(value)
-                                        FROM aggregation_step
-                                        WHERE operation_id=? AND round=? AND agg_func='count'
-                                        HAVING count(value)=?''', (operation_id, exec_round, client_count))
+    if agg_func == 'count' or agg_func == AGG.AVG.value:
+        cursor.execute(global_aggregation, (operation_id, exec_round,AGG.COUNT.value, client_count))
         result = cursor.fetchone()
         if result:
             _count = result[0]
@@ -103,11 +104,11 @@ def get_aggregation():
     connection.close()
     print("Count",_count)
     print("Sum",_sum)
-    if agg_func == 'count' and _count is not None:
+    if agg_func == AGG.COUNT.value and _count is not None:
         return jsonify(_count), 200
-    elif agg_func == 'avg' and _count is not None and _sum is not None:
+    elif agg_func == AGG.AVG.value and _count is not None and _sum is not None:
         return jsonify(_sum/_count), 200
-    elif agg_func == 'sum' and _sum is not None:
+    elif agg_func == AGG.SUM.value and _sum is not None:
         return jsonify(_sum ), 200
     else:
         return jsonify(None), 300
