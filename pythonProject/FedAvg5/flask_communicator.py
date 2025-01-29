4ae42560a5cce6f3fdd045e6115da9dafbb7d7e2
thoubou <thoubou@gmail.com>
2025-01-29 15:18:27 +0200

    Update flask_communicator.py

--- pythonProject/FedAvg5/flask_communicator.py
+++ pythonProject/FedAvg5/flask_communicator.py
@@ -1,4 +1,5 @@
 import requests
+from constants import Database,AGG
 
 class FlaskCommunicator:
     def __init__(self):
@@ -8,17 +9,21 @@ class FlaskCommunicator:
     @staticmethod
     def add_aggregation(operation_id, client_id, agg_round, agg_func, value):
         url = "http://localhost:5000/add-aggregation"
-        response = requests.post(url, params={"operation_id": operation_id,"client_id":client_id,
-                                              "round":agg_round, "agg_func":agg_func, "value":value})
+        response = requests.post(url, params={Database.OPP.value: operation_id,
+                                              Database.CLIENT.value:client_id,
+                                              Database.ROUND.value:agg_round,
+                                              Database.AGG.value:agg_func,
+                                              Database.VALUE.value:value})
         if response.status_code == 200:
             print("Success:", response.text)
 
     @staticmethod
     def get_aggregation(operation_id, exec_round, agg_func, client_count):
         url = "http://localhost:5000/get_aggregation"
-        response = requests.get(url, params={"operation_id": operation_id,
-                                             "round":exec_round, "agg_func":agg_func,
-                                             "client_count":client_count})
+        response = requests.get(url, params={Database.OPP.value: operation_id,
+                                             Database.ROUND.value:exec_round,
+                                             Database.AGG.value:agg_func,
+                                             Database.CLIENT_COUNT.value:client_count})
         if response.status_code == 200:
             print("Success:", response.text)
         return response.text
