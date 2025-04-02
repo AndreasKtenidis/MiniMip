38449d8103b74fc3d2e6de54da5e983c32a980bd
thoubou <thoubou@gmail.com>
2025-04-02 15:40:23 +0300

    Removing unecessary

--- pythonProject/FedAvg6/client/aggregation_client.py
+++ pythonProject/FedAvg6/client/aggregation_client.py
@@ -15,23 +15,3 @@ class NumpyAggregationClient(ABC):
     @abstractmethod
     def __global_max__(self, local_max):
         pass
-
-    def sum(self, a):
-        _ans = self.__global_sum__(np.stack([np.sum(np.asarray(a))], axis=0))
-        return _ans[0]
-
-    def count(self, a):
-        _ans = self.__global_sum__(np.stack([len(np.asarray(a))], axis=0))
-        return _ans[0]
-
-    def avg(self, a):
-        _ans = self.__global_sum__(np.stack([np.sum(np.asarray(a)), len(np.asarray(a))], axis=0))
-        return _ans[0]/_ans[1]
-
-    def min(self, a):
-        _ans = self.__global_min__(np.stack([np.min(np.asarray(a))], axis=0))
-        return _ans[0]
-
-    def max(self, a):
-        _ans = self.__global_max__(np.stack([np.max(np.asarray(a))], axis=0))
-        return _ans[0]
