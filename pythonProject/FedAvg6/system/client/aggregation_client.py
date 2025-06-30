added new statistical function to pandas vs grizzly and fixed a bug in the computation of global_avg of grizzly agg client

pythonProject/FedAvg6/system/client/aggregation_client.py
@@ -403,7 +403,9 @@ class GrizzlyAggClient( ABC):
     def global_avg(self, dataframe):
         _agg = dataframe.mean()
         means = [row[1] for row in _agg.collect()]
-        means_with_count = means + [dataframe.count().collect()[0][1]]
+        counts_df = dataframe.count()
+        counts = [row[1] for row in counts_df.collect()]
+        means_with_count = means + counts
         _ans = self.client.__global_sum__(means_with_count)
         _ans = np.array(_ans[0:len(_ans)//2]) / _ans[len(_ans)//2:]
         return _ans