a99666e7e51d17b6c53eae0b0ab612e6b0ffe0a3
thoubou <thoubou@gmail.com>
2025-09-09 13:14:14 +0300

    Step 1. removing previous version
    Step 1. removing previous version

--- tests/library_tests/grizzly_pearson/client1.py
+++ tests/library_tests/grizzly_pearson/client1.py
@@ -1,2 +1,25 @@
-from tests.library_tests.grizzly_pearson.client_impl import compute
-compute(0)
+import sys
+
+from tests.help_datasets.blob import BlobDataset
+from tests.library_tests.grizzly_pearson.pearson_test import PearsonTest
+from tests.library_tests.k_means_rand_Pandas.kmeans_test_pandas import KmeansPandasTest
+
+
+def main():
+    aggregation_server= None
+    if len(sys.argv) < 2:
+        aggregation_server="localhost:50051"
+    else:
+        aggregation_server = sys.argv[1]
+        print(aggregation_server)
+
+
+    PearsonTest(0, 2,
+                           dataset=BlobDataset(),
+                           operation_id=0,
+                           aggregation_server=aggregation_server)
+
+
+
+if __name__ == "__main__":
+    main()
